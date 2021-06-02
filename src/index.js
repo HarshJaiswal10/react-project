91b1470577134721a0d9f8a9cd2ff54d6c6d4cf7
Harsh Jaiswal <harshj254@gmail.com>
2021-06-02 01:15:57 -0700

    day 3 completed

--- src/index.js
+++ src/index.js
@@ -1,30 +1,18 @@
-// var React = require ('react'); 
-import React from 'react'; //modern js with babel
-// var ReactDOM = require ('react-dom');
-import ReactDOM from 'react-dom'; 
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// ReactDOM.render('kya dikhana hai','kaha dikhana hai','callback func'); 
 ReactDOM.render(
-  // <div>
-  //   <h1> Harsh Jaiswal!! </h1>
-  //   <p>Learning react.js </p>
-  //   </div> ,
-    // ---------------- 
-  // [
-  //   <h1> Harsh Jaiswal!! </h1>,
-  //   <p>Learning react.js </p>
-  // ] ,  
-
-  // <React.Fragment>
-  //    <h1> Harsh Jaiswal!! </h1>
-  //    <p>Learning react.js </p>
-  //    </React.Fragment> , 
-
-  // we also use react fragment as <>
   <>
-     <h1> Harsh Jaiswal!! </h1>
-     <p>Learning react.js </p>
-     </> , 
+  <h1>Harsh Jaiswal Netflix</h1>
+  <p>Top 5 Netflix series</p>
+  <ol>
+    <li>Bracking Bad</li>
+    <li>Money Heist</li>
+    <li>Dark</li>
+    <li>you</li>
+    <li>Stranger Things</li>
+  </ol>
+  </>,
+  document.getElementById("root")
 
-    document.getElementById("root")
-); 
+);
