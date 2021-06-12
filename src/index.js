fc44c04c6a3c206963788ba1addd9aa9681a88b3
Harsh Jaiswal <harshj254@gmail.com>
2021-06-12 02:15:23 -0700

    day 7 complete

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
- 
-const fname = 'Harsha';
-const lname = 'Jaiswal';
-const d = new Date().toLocaleDateString();
-const t = new Date().toLocaleTimeString();
 
+const name = "Harsh jaiswal";
+const img1 = "https://picsum.photos/200/300";
+const img2 = "https://picsum.photos/300/300";
+const img3 = "https://picsum.photos/400/300";
+const link = "https://www.google.co.in/";
 ReactDOM.render(
   <>
-  <h1>{`My name is ${fname} ${lname}`}</h1>
-  {/* <p>{`Todays date is ${d.getDate()} / ${d.getMonth()} / ${d.getFullYear()}`}</p> */}
-  <p>{`Todays date is ${d}`}</p>
-  <p>{`Current Time is ${t}`}</p>
+      <h1 contentEditable = "true">My name is {name}</h1>
+      <img src = {img1} alt="randomImages" />
+      <img src = {img2} alt="randomImages" />
+      <a href = {link} target="_harsh" >
+       <img src = {img3} alt="randomImages" />
+       </a>
   </>,
   document.getElementById("root")
-  );
+);
