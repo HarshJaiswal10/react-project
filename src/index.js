4819b19223ea1dc3fa567835efb51c0dc0885904
Harsh Jaiswal <harshj254@gmail.com>
2021-06-12 01:12:27 -0700

    day 6 complete

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom';
  
 const fname = 'Harsha';
 const lname = 'Jaiswal';
+const d = new Date().toLocaleDateString();
+const t = new Date().toLocaleTimeString();
 
 ReactDOM.render(
-  <h1>{`My name is ${fname} ${lname}`}</h1>,
+  <>
+  <h1>{`My name is ${fname} ${lname}`}</h1>
+  {/* <p>{`Todays date is ${d.getDate()} / ${d.getMonth()} / ${d.getFullYear()}`}</p> */}
+  <p>{`Todays date is ${d}`}</p>
+  <p>{`Current Time is ${t}`}</p>
+  </>,
   document.getElementById("root")
   );
