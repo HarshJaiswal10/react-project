719055faacc49bbbd99c54c4c5938705ee0788b8
Harsh Jaiswal <harshj254@gmail.com>
2021-06-16 00:09:32 -0700

    day 10 complete

---
+++ src/Ol.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Ol(){
+    return (<ol>
+        <li>Harsh</li>
+        <li>Gourav</li>
+        <li>Ashwin</li>
+        <li>Abhi</li>
+      </ol>);
+}
+
+export default Ol;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,7 @@
 import React from 'react';
-import reactDom from 'react-dom';
-import './index.css';
-
-let curDate = new Date(2020, 6, 6, 7);
-curDate = curDate.getHours();
-let greeting ='';
-const cssStyle ={};
-
-if(curDate>= 6 && curDate <12){
-  greeting = 'Good Morning';
-  cssStyle.color = 'blue';
-} else if(curDate>=12 && curDate <19){
-  greeting = 'Good Afternoon';
-  cssStyle.color = 'orange';
-}else{
-  greeting = 'Good Night';
-  cssStyle.color = 'black';
-}
-
-reactDom.render(
-  <>
-  <div>
-  <h1>Hello Sir, <span style={cssStyle}>{greeting}</span></h1>,
-  </div>
-  </>,
+import ReactDOM from 'react-dom';
+import App from './App';
+ReactDOM.render(
+    <App />,
   document.getElementById("root")
 );
