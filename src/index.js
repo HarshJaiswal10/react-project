a851213e28ce866e2fcb8760a9d3dddf33dc7bed
Harsh Jaiswal <harshj254@gmail.com>
2021-06-19 01:45:00 -0700

    day 12 complete

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import add from './Calculator';
-import {sub, div, mult} from './Calculator';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Card from './Card';
 
 ReactDOM.render(
   <>
-    <ul>
-      <li> Sum of two no is {add(40, 4)} </li>
-      <li> Sub of two no is{sub(40,4)}</li>
-      <li> Div of two no is{div(40,4)}</li>
-      <li> Mult of two no is{mult(40,4)}</li>
-    </ul>
+    <Card 
+      imgsrc="https://wallpapercave.com/wp/wp4056410.jpg"
+      title="A Netflix Original Series"
+      sname="Dark"
+      link="https://www.netflix.com/in/tittle/80990668?source=35"
+     />
+    <Card 
+      imgsrc="https://m.media-amazon.com/images/M/MV5BNGQxMDcyOTEtZWZkNi00NzgwLWEzNjQtZmFhMGY5ZGRlMTdiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
+      title="A Netflix Original Series"
+      sname="Extra curricular"
+      link="https://www.netflix.com/in/title/80990668"
+     />
+    <Card 
+      imgsrc="https://m.media-amazon.com/images/M/MV5BN2ZmYjg1YmItNWQ4OC00YWM0LWE0ZDktYThjOTZiZjhhN2Q2XkEyXkFqcGdeQXVyNjgxNTQ3Mjk@._V1_.jpg"
+      title="A Netflix Original Series"
+      sname="Stranger things"
+      link="https://www.netflix.com/in/title/80057281"
+     />
   </>,
   document.getElementById("root")
 );
