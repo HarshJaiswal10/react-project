17f0f4d3caf5cc3c8209d310738f1f287b60c61f
Harsh Jaiswal <harshj254@gmail.com>
2021-06-22 02:04:10 -0700

    day 13 complete

---
+++ src/Sdata.jsx
@@ -0,0 +1,34 @@
+const Sdata = [
+    {
+        imgsrc:"https://wallpapercave.com/wp/wp4056410.jpg",
+        title:"A Netflix Original Series",
+        sname:"Dark",
+        link: "https://www.netflix.com/in/title/80100172"
+    },
+    {
+        imgsrc:"https://m.media-amazon.com/images/M/MV5BNGQxMDcyOTEtZWZkNi00NzgwLWEzNjQtZmFhMGY5ZGRlMTdiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
+        title : "A Netflix Original Series",
+        sname:"Extra curricular",
+        link:"https://www.netflix.com/in/title/80990668"
+    },
+    {
+        imgsrc:"https://m.media-amazon.com/images/M/MV5BN2ZmYjg1YmItNWQ4OC00YWM0LWE0ZDktYThjOTZiZjhhN2Q2XkEyXkFqcGdeQXVyNjgxNTQ3Mjk@._V1_.jpg",
+        title:"A Netflix Original Series",
+        sname:"Stranger things",
+        link:"https://www.netflix.com/in/title/80057281"
+    },
+    {
+        imgsrc:"https://www.thebulletintime.com/wp-content/uploads/2021/04/Money-Heist-Season-5-2.jpg",
+        title:"A Netflix Original Series",
+        sname:"Money Heist",
+        link:"https://www.netflix.com/in/title/80192098"
+    },
+    {
+        imgsrc:"https://i.redd.it/fxok1mzqgar51.jpg",
+        title:"A Netflix Original Series",
+        sname:"The Witcher",
+        link:"https://www.netflix.com/in/title/80189685"
+    }
+];
+
+export default Sdata;
--- src/index.js
+++ src/index.js
@@ -1,27 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Card from './Card';
+import "./index.css";
+import Sdata from "./Sdata";
+
+// function ncard(val){
+//   return(
+//     <Card 
+//       imgsrc={val.imgsrc}
+//       title={val.title}
+//       sname={val.sname}
+//       link={val.link}
+//      />
+//   );
+// } 
 
 ReactDOM.render(
   <>
-    <Card 
-      imgsrc="https://wallpapercave.com/wp/wp4056410.jpg"
-      title="A Netflix Original Series"
-      sname="Dark"
-      link="https://www.netflix.com/in/tittle/80990668?source=35"
-     />
-    <Card 
-      imgsrc="https://m.media-amazon.com/images/M/MV5BNGQxMDcyOTEtZWZkNi00NzgwLWEzNjQtZmFhMGY5ZGRlMTdiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
-      title="A Netflix Original Series"
-      sname="Extra curricular"
-      link="https://www.netflix.com/in/title/80990668"
-     />
-    <Card 
-      imgsrc="https://m.media-amazon.com/images/M/MV5BN2ZmYjg1YmItNWQ4OC00YWM0LWE0ZDktYThjOTZiZjhhN2Q2XkEyXkFqcGdeQXVyNjgxNTQ3Mjk@._V1_.jpg"
-      title="A Netflix Original Series"
-      sname="Stranger things"
-      link="https://www.netflix.com/in/title/80057281"
-     />
+
+    <h1 className="heading_style"> List of top 5 Netflix series</h1>
+
+    {Sdata.map((val)=>{
+      return(
+        <Card 
+          imgsrc={val.imgsrc}
+          title={val.title}
+          sname={val.sname}
+          link={val.link}
+        />
+      );
+    })}
   </>,
   document.getElementById("root")
 );
