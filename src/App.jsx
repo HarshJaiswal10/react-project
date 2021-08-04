bd12f3103ccb0c32975d00f3ae924a153e8b2022
Harsh Jaiswal <harshj254@gmail.com>
2021-08-04 12:18:07 -0700

    day 18 complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,26 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  const state = useState();
-  const [count, setCount] = useState(0);
+  
+  const[name, setName] = useState();
+  const[fullName, setFullName] = useState();
 
-  const IncNum = () => {
-    setCount(count + 1); 
+  const InputEvent = (event) => {
+    setName(event.target.value);
+  };
+  const onSubmit = () => {
+    setFullName(name);
   };
 
   return(
-    <>
-      <h1> {count} </h1>
-      <button onClick={IncNum}> Click Me</button>
-    </>
-  ); 
+  <>
+    <div>
+      <h1>hello {fullName}</h1>
+      <input type='text' placeholder='Enter Your Name' onChange={InputEvent} value={name} />
+      <button onClick={onSubmit}>Submit</button>
+    </div>
+  </>
+  );
 };
-
-export default App;
-
-// import React, { useState } from 'react';
-
-// const App = () => {
-//   let newTime = new Date().toLocaleTimeString();
-//   const [ctime, setCTime] = useState(newTime);
-
-//   const IncNum = () => {
-//     let newcTime = new Date().toLocaleTimeString();
-//     setCTime(newcTime); 
-//   };
-
-//   return(
-//     <>
-//       <h1> {ctime} </h1>
-//       <button onClick={IncNum}> Click Me</button>
-//     </>
-//   ); 
-// };
 
 export default App;
