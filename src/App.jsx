f58bcdb2338f5c4f5fcf2bd72f69601b6fd33241
Harsh Jaiswal <harshj254@gmail.com>
2021-08-05 00:26:52 -0700

    day 18.1 complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,34 @@ import React, { useState } from 'react';
 
 const App = () => {
   
-  const[name, setName] = useState();
-  const[fullName, setFullName] = useState();
+  const[name, setName] = useState("");
+  const[fullName, setFullName] = useState("");
+  const[lastName, setLastName] = useState("");
+  const[lastNameNew, setLastNameNew] = useState("");
 
   const InputEvent = (event) => {
     setName(event.target.value);
   };
-  const onSubmit = () => {
+  const onSubmit = (event) => {
+    event.preventDefault();
     setFullName(name);
+    setLastNameNew(lastName)
+  };
+  const InputEventTwo = (event) =>{
+    setLastName(event.target.value);
   };
 
   return(
   <>
+    <form onSubmit={onSubmit}>
     <div>
-      <h1>hello {fullName}</h1>
+      <h1>hello {fullName} {lastNameNew}</h1>
       <input type='text' placeholder='Enter Your Name' onChange={InputEvent} value={name} />
-      <button onClick={onSubmit}>Submit</button>
+      <br />
+      <input type='text' placeholder='Enter Your Last Name' onChange={InputEventTwo} value={lastName} />
+      <button type="submit">Submit</button>
     </div>
+    </form>
   </>
   );
 };
