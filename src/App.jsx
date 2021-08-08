8bfa6ee59b8e8c4cfafa087e1d49b575eaeea17f
Harsh Jaiswal <harshj254@gmail.com>
2021-08-08 00:23:37 -0700

    day 19 complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,67 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  const state = useState();
-  const [count, setCount] = useState(0);
+  
+  const[fullName, setFullName] = useState({
+    fname : '',
+    lname : '',
+  });
 
-  const IncNum = () => {
-    setCount(count + 1); 
+  const InputEvent = (event) => {
+    // setFullName(event.target.value);
+    console.log(event.target.value);
+    console.log(event.target.name);
+
+    // const value = event.target.value;
+    // const name = event.target.name;
+
+    const {value, name} = event.target;
+
+    setFullName((preValue) => {
+      // console.log(preValue); 
+      if(name === 'fname'){
+       return{
+          fname : value,
+          lname : preValue.lname,
+       };
+      } else if(name === 'lname'){
+        return{
+           fname : preValue.fname,
+           lname : value,
+        };
+       }
+    });
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    alert('Form Submitted'); 
   };
 
   return(
-    <>
-      <h1> {count} </h1>
-      <button onClick={IncNum}> Click Me</button>
-    </>
-  ); 
+  <>
+    <form onSubmit={onSubmit}>
+    <div>
+      <h1>hello {fullName.fname} {fullName.lname}</h1>
+      <input 
+        type='text' 
+        placeholder='Enter Your Name' 
+        name="fname"
+        onChange={InputEvent} 
+        value={fullName.fname} 
+      />
+      <br />
+      <input 
+        type='text' 
+        placeholder='Enter Your Last Name' 
+        name="lname"
+        onChange={InputEvent} 
+        value={fullName.lname} 
+      />
+      <button type="submit">Submit</button>
+    </div>
+    </form>
+  </>
+  );
 };
-
-export default App;
-
-// import React, { useState } from 'react';
-
-// const App = () => {
-//   let newTime = new Date().toLocaleTimeString();
-//   const [ctime, setCTime] = useState(newTime);
-
-//   const IncNum = () => {
-//     let newcTime = new Date().toLocaleTimeString();
-//     setCTime(newcTime); 
-//   };
-
-//   return(
-//     <>
-//       <h1> {ctime} </h1>
-//       <button onClick={IncNum}> Click Me</button>
-//     </>
-//   ); 
-// };
 
 export default App;
